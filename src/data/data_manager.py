222872f329f73905db57225f6c3c0738cbb4506f
LEEKIWOOK <leek0502@gmail.com>
2021-11-29 13:53:23 +0900

    Spearman corr = 0.727844555510926

--- src/data/data_manager.py
+++ src/data/data_manager.py
@@ -51,11 +51,12 @@ class DataManager:
 
         
         data = pickle.load(open(self.domain_file[int(self.target_list[0])], "rb"))
+        
+        
 
         data_size = len(data["X"])
         indice = list(range(data_size))
 
-        # np.random.seed(self.random_seed)
         np.random.shuffle(indice)
 
         minY = min(data["Y"])
@@ -95,100 +96,6 @@ class DataManager:
         }
         return train_set, valid_set, test_set
 
-    def random_sort(self, data):
-
-        data_list = {"X": list(), "Y": list()}
-        for j in "X", "Y":
-            for i in range(len(data)):
-                data_list[j] += data[i][j]
-            if j == "X":
-                indices = list(range(len(data_list["X"])))
-                Xindices = random.sample(indices, len(indices))
-
-        data_set = {
-            "X": [data_list["X"][k] for k in Xindices],
-            "Y": [data_list["Y"][k] for k in Xindices],
-            #"S": [data_list["S"][k] for k in Xindices],
-        }
-
-        return data_set
-
-    def merge_load(self, ratio=1.0):
-        train = list()
-        valid = list()
-        test = list()
-
-        for idx in self.target_list:
-            self.target_domain.append(self.domain_list[int(idx)])
-            self.target_file.append(self.domain_file[int(idx)])
-
-            data = pickle.load(open(self.domain_file[int(idx)], "rb"))
-            data_size = len(data["X"])
-            indice = list(range(data_size))
-
-            test_ratio = 0.15
-            val_ratio = test_ratio
-            test_size = int(np.floor(data_size * test_ratio))
-            tv_size = int(np.floor(data_size * (1 - test_ratio) * ratio))
-            train_size = int(np.floor(tv_size * (1 - val_ratio)))
-            valid_size = int(np.floor(tv_size * val_ratio))
-
-            indices = dict()
-            indices["Val"] = random.sample(indice[:valid_size], valid_size)
-            indices["Test"] = random.sample(
-                indice[valid_size : valid_size + test_size], test_size
-            )
-            indices["Train"] = random.sample(
-                indice[valid_size + test_size : valid_size + test_size + train_size],
-                train_size,
-            )
-            mean_y = sum(data["Y"], 0.0) / len(data["Y"])
-            #data["Y"] = [data["Y"][i]/mean_y for i in range(len(data["Y"]))]
-
-            train_set = {
-                "X": [data["X"][i] for i in indices["Train"]],
-                "Y": [data["Y"][i]/mean_y for i in indices["Train"]],
-                #"S": [int(idx) for i in indices["Train"]],
-            }
-            test_set = {
-                "X": [data["X"][i] for i in indices["Test"]],
-                "Y": [data["Y"][i]/mean_y for i in indices["Test"]],
-                #"S": [int(idx) for i in indices["Test"]],
-            }
-            valid_set = {
-                "X": [data["X"][i] for i in indices["Val"]],
-                "Y": [data["Y"][i]/mean_y for i in indices["Val"]],
-                #"S": [int(idx) for i in indices["Val"]],
-            }
-
-            train.append(train_set)
-            valid.append(valid_set)
-            test.append(test_set)
-
-        ret_y = (
-            train[0]["Y"]
-            + train[1]["Y"]
-            + train[2]["Y"]
-            + valid[0]["Y"]
-            + valid[1]["Y"]
-            + valid[2]["Y"]
-            + test[0]["Y"]
-            + test[1]["Y"]
-            + test[2]["Y"]
-        )
-        minY = min(ret_y)
-        maxY = max(ret_y)
-        for i in range(0, 3):
-            train[i]["Y"] = [(x - minY) / (maxY - minY) for x in train[i]["Y"]]
-            valid[i]["Y"] = [(x - minY) / (maxY - minY) for x in valid[i]["Y"]]
-            test[i]["Y"] = [(x - minY) / (maxY - minY) for x in test[i]["Y"]]
-
-        train_set = self.random_sort(train)
-        valid_set = self.random_sort(valid)
-        test_set = self.random_sort(test)
-
-        return train_set, valid_set, test_set
-
     def loader_only(self, data):
 
         loader = DataLoader(
@@ -205,7 +112,11 @@ class DataWrapper:
     def __init__(self, data, transform=None):
         self.data = data
         self.transform = transform
-        self.nuc_to_idx = {"A": 1, "C": 2, "G": 3, "T": 4}
+        self.kmer = 3
+        self.kmer_stride = 1
+        nucleotide = ['A', 'C', 'G', 'T']
+        table_key = [x+y+z for x in nucleotide for y in nucleotide for z in nucleotide]
+        self.table = {key:idx for idx, key in enumerate(table_key)}
 
     def __len__(self):
         return len(self.data["X"])
@@ -215,11 +126,13 @@ class DataWrapper:
             idx = idx.to_list()
 
         res = dict()
+        rlist = list()
         for col in self.data.keys():
             if col == "X":
-                res[col] = torch.tensor(
-                    [self.nuc_to_idx[x] for x in self.data[col][idx]], dtype=torch.long
-                )
+                for i in range(len(self.data[col][idx]) - (self.kmer - 1)):
+                  char = ''.join(self.data[col][idx][i:i + self.kmer])
+                  rlist.append(self.table.get(char, -1))
+                res[col] = torch.tensor(rlist, dtype=torch.long)
             else:
                 res[col] = torch.tensor(self.data[col][idx], dtype=torch.float)
         return res
